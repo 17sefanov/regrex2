8f5c74530ee36ed7555d2d0b5e58a9ae7b7583e2
17sefanov <86427242+17sefanov@users.noreply.github.com>
2021-07-08 12:59:31 -0400

    Create r_plot.r

---
+++ r_plot.r
@@ -0,0 +1,14 @@
+args <- commandArgs(trailingOnly = TRUE)
+regrex <- read.csv(args[1])
+
+library(ggplot2)
+df=read.csv("regrex1.csv")
+df
+
+png('r_orig.png')
+ggplot(data=df, aes(x=x, y=y))+geom_point()
+dev.off()
+
+png('r_lm.png')
+ggplot(data=df, aes(x=x, y=y))+geom_point()+geom_smooth(method=lm)
+dev.off()
